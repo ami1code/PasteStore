cb079ff32ed0c166bbe70f1585ab76bfc5dcc108
ami1code <amit.kdas.eee24@itbhu.ac.in>
2025-12-25 08:43:47 +0530

    First commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+
+import { NavbarData } from "../data/Navbar";
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="w-full h-[60px] flex justify-center items-center p-4 bg-blue-950 gap-x-10">
+      {NavbarData.map((link, idx) => (
+        <NavLink
+          key={idx}
+          to={link.path}
+          className={({ isActive }) =>
+            isActive
+              ? "text-blue-500 font-bold text-2xl hover:text-blue-300"
+              : "text-white font-bold text-2xl hover:text-blue-300"
+          } 
+        >
+          {link.title}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+export default Navbar;
